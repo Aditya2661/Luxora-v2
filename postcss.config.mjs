a73beb432be388385e2bc67b7f3f58b1e580af4e
Aditya Pandey <22053219@kiit.ac.in>
2025-05-31 00:57:05 +0530

    resolving errors

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-module.exports = {
+// postcss.config.mjs (ES Modules)
+export default {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
-
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// app/layout.js or app/layout.tsx
+import './globals.css';
+
 import Navbar from "./Navbar";
 import Footer from './Footer'
 import SplashScreen from './SplashScreen'
